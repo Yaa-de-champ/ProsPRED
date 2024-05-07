49b453c025c0d9a300ff341538a102588568ddb5
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2024-05-07 14:36:24 +0000

    more changes

--- django-website/prospred/urls.py
+++ django-website/prospred/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]
