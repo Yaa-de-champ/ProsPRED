e4d507e16b12731903b49657bf2d7d96a4dbf522
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2024-05-29 10:56:58 +0000

    'changes

--- django-website/prospred/views.py
+++ django-website/prospred/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.http import JsonResponse
+import numpy as np
+from .forms import ImageUploadForm
+# from .models import load_model
 # Create your views here.
 # def home(request):
 #     return HttpResponse('Hello World')
@@ -16,3 +19,11 @@ def register(request):
     # perform registration logic here
         pass
     return render (request, 'register.html')
+
+# def predict(request):
+#     if request.method == 'POST':
+#         form = ImageUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             file = request.FILES['image']
+            
+            
